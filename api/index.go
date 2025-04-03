a4fcf5c0e5bcfa19a0e02956922257d729a41370
Victor Amano Izawa <victorsjc@gmail.com>
2025-04-03 07:52:30 -0300

    atualizacao do uso do Gin

--- api/index.go
+++ api/index.go
@@ -4,24 +4,46 @@ import (
  "encoding/json"
  "net/http"
 
- "github.com/gorilla/mux"
+ "github.com/gin-contrib/cors"
+ "github.com/gin-gonic/gin" 
 )
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
- dataJson := map[string]interface{}{
-  "data": map[string]interface{}{
-   "message": "Hello World!",
-  },
- }
+const KN_SECURITY_HOLDER = "security_holder"
+const KN_AUTHORIZATION = "Authorization"
 
- err := json.NewEncoder(w).Encode(dataJson)
- if err != nil {
-  http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
- }
+func HealthIndicatorHandler(c *gin.Context) {
+    //talvez o ideal seria verificar o status do banco de dados
+    c.JSON(http.StatusOK, gin.H{
+        "status": "Up",
+    })
 }
 
-func Serve() {
- r := mux.NewRouter()
- r.HandleFunc("/", HomeHandler).Methods("GET")
- http.ListenAndServe(":8080", r)
+func Serve(){
+    r := gin.Default()
+    // Configuração do CORS
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:5000"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+
+    // Middleware para verificar o token
+    r.Use(func(c *gin.Context) {
+        token := c.GetHeader(KN_AUTHORIZATION)
+        if token == "" {
+            c.JSON(http.StatusUnauthorized, gin.H{"error": "Token não fornecido"})
+            c.Abort()
+            return
+        }
+        c.Next()
+    })
+
+
+    r.GET("/health", HealthIndicatorHandler)
+
+    ginLambda = ginadapter.New(r)
+    r.Run(":8080")    
 }
