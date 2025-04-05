9497c07dcd6b6e0425156ba297f78c6a1acec8ca
Victor Amano Izawa <victorsjc@gmail.com>
2025-04-05 13:50:56 -0300

    atualizacao checkin

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,18 @@
 package handler
 
 import (
+	"encoding/json"	
 	"net/http"
 	"github.com/gin-gonic/gin"
 )
+
+type RequestCheckin struct {
+	id string `json:"id"`
+	date   string `json:"date"`
+	direction string `json:"direction"`
+	returnTime   string `json:"returnTime"`
+	status string `json:"status"`
+}
 
 func Health(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status":"Up"})
@@ -14,7 +23,14 @@ func Register(c *gin.Context) {
 }
 
 func Checkin(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"id":"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855","date":"","direction":"","status":"REGISTERED"})
+	var checkin RequestCheckin
+	//c.JSON(http.StatusOK, gin.H{"id":"e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855","date":"","direction":"","status":"REGISTERED"})
+    if err := c.BindJSON(&ncheckin); err != nil {
+        return
+    }
+    checkin.id = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+    checkin.status = "REGISTERED"
+	c.IndentedJSON(http.StatusCreated, checkin)
 }
 
 func ErrRouter(c *gin.Context) {
