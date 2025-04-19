3241781c7257ddd88e580aafa8b3a5ba49fc4edb
Victor Amano Izawa <victorsjc@gmail.com>
2025-04-18 23:09:02 -0300

    atualizacao login

--- api/entrypoint.go
+++ api/entrypoint.go
@@ -12,9 +12,9 @@ var (
 
 func registerRouter(r *gin.RouterGroup) {
     r.GET("/api/health", handler.Health)
-    r.POST("/api/auth/token", handler.RealizarSocialLogin)
+    r.GET("/api/auth/token", handler.RealizarSocialLogin)
     r.OPTIONS("/api/auth/token", func(c *gin.Context) {
-        c.Header("Access-Control-Allow-Origin", "https://ui-transportecheckin-app.vercel.app", "https://transportecheckin-services-victorsjc-victorsjcs-projects.vercel.app")
+        c.Header("Access-Control-Allow-Origin", "https://ui-transportecheckin-app.vercel.app, https://transportecheckin-services-victorsjc-victorsjcs-projects.vercel.app")
         c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
         c.Header("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
         c.AbortWithStatus(200)
